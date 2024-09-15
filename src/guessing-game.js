ad34cef680ad69e03bb957b56dc06af2f6165b63
Californiae <1288217@ukr.net>
2024-09-15 17:13:10 +0200

    init: implement guessing

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,31 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+        this.min = null;  // Минимальная граница диапазона
+        this.max = null;  // Максимальная граница диапазона
+        this.currentGuess = null;  // Текущее предположение
+      }
+    
+      // Метод для установки диапазона чисел
+      setRange(min, max) {
+        this.min = min;
+        this.max = max;
+      }
+    
+      // Метод для предположения (вычисляем среднее число в диапазоне)
+      guess() {
+        this.currentGuess = Math.ceil((this.min + this.max) / 2);
+        return this.currentGuess;
+      }
+    
+      // Метод вызывается, если предположение больше загаданного числа
+      lower() {
+        this.max = this.currentGuess;
+      }
+    
+      // Метод вызывается, если предположение меньше загаданного числа
+      greater() {
+        this.min = this.currentGuess;
+      }
     }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
-}
 
 module.exports = GuessingGame;
